17ca425df271b5bd761df0e822c0179a7871b212
Allan Heremi <allanheremidev@gmail.com>
2023-07-17 13:11:59 +0200

    Fix: Fixed Prisma connection limit to 1

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,5 +1,17 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export default prisma;
+
+export default prisma
+
+/* 
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalForPrisma = prisma;
+ */
+
+
+
