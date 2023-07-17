e14ee2d9eb51298935c0ce2bc376082a25154470
Allan Heremi <allanheremidev@gmail.com>
2023-07-17 10:15:53 +0200

    Fix: ./app.ts changed to ./app.tsx, bugfix

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,7 +1,8 @@
+import type { AppProps }from 'next/app';
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps } : AppProps) {
   return (
     <UserProvider>
       <Component {...pageProps} />
