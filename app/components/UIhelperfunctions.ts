267fcd63333dd2ae2760b53a81f5d152351b5703
Allan Heremi <allanheremidev@gmail.com>
2023-07-20 09:52:59 +0200

    Feat: Created UIhelperfunction for header

---
+++ app/components/UIhelperfunctions.ts
@@ -0,0 +1,31 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+export function randomizeLetters(target: HTMLHeadingElement, intervalTime: number = 30): void {
+  let interval: NodeJS.Timeout | null = null;
+  let iteration = 0;
+
+  if (interval) {
+    clearInterval(interval);
+  }
+
+  interval = setInterval(() => {
+    target.innerText = target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return target.dataset.value?.[index] ?? letter;
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= (target.dataset.value?.length ?? 0)) {
+      if (interval) {
+        clearInterval(interval);
+      }
+    }
+
+    iteration += 1 / 3;
+  }, intervalTime);
+}
