a50da215ca4a0b2f99b70eb5de52d0d40642bbca
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-21 13:57:54 +0200

    refactor: remove stuff

--- app/recipes/AddIngredientForm.tsx
+++ app/recipes/AddIngredientForm.tsx
@@ -1,30 +1,24 @@
-import { Recipe_ingredient, addIngredient } from '@/types';
-import { useEffect, useState } from 'react';
-import { updateIngredient } from '../db/prisma';
+import { addIngredient } from '@/types';
+import { useState } from 'react';
 
 const AddIngredientForm = () => {
   const data = localStorage.getItem('ingList') as string;
   const currentList = JSON.parse(data);
 
-  const [ingName, setIngname] = useState<string>('');
+  const [name, setName] = useState<string>('');
   const [quantity, setQuantity] = useState<number>(0);
   const [unit, setUnit] = useState<string>('');
-  const [ingList, setIngList] = useState<addIngredient[]>(currentList);
+  const [ingList, setIngList] = useState<addIngredient[]>([]);
 
-  const updatedIngredient = {
-    name: ingName,
-    quantity: quantity,
-    unit: unit,
-  };
   return (
     <>
       <div>
         <label>Ingredient Name:</label>
         <input
           type="text"
-          value={ingName}
+          value={name}
           onChange={e => {
-            setIngname(e.target.value);
+            setName(e.target.value);
           }}
         />
         <label>Quantity:</label>
@@ -40,12 +34,11 @@ const AddIngredientForm = () => {
           onChange={e => setUnit(e.target.value)}
         />
         <button
-          type="submit"
           onClick={e => {
-            ingList.push(updatedIngredient);
             e.preventDefault();
+            setIngList([...ingList, { name, quantity, unit }]);
             localStorage.setItem('ingList', JSON.stringify(ingList));
-            setIngname('');
+            setName('');
             setQuantity(0);
             setUnit('');
           }}
