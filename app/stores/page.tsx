7cbb0aa8a900c9184cf15651faa8ee2eb930a1c2
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-22 19:36:26 +0200

    feat: show stores

--- app/stores/page.tsx
+++ app/stores/page.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
+'use client';
+import { Store } from '@/types';
+import React, { useEffect, useState } from 'react';
+import * as stores from '../db/stores';
+import Link from 'next/link';
 
-type Props = {}
-
-const Stores = (props: Props) => {
+const Stores = () => {
+  const [storesState, setStoresState] = useState<Store[]>([]);
+  const [name, setName] = useState('');
+  useEffect(() => {
+    stores.getAll().then(s => setStoresState(s));
+  }, []);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await stores.addDefault(name);
+    setStoresState(await stores.getAll());
+  };
   return (
-    <div>Stores</div>
-    
-  )
-}
+    <>
+      <div>Stores</div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </form>
+      <ul>
+        {storesState.map(s => (
+          <li key={s.id}>
+            <Link href={`/stores/${s.id}`}>{s.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
-export default Stores
+export default Stores;
