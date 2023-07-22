3e2787afa69176d26d22d5fcdf9ea5859ff0c807
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-22 19:29:52 +0200

    fix: remove fragment

--- app/components/DeleteButton.tsx
+++ app/components/DeleteButton.tsx
@@ -4,14 +4,12 @@ type Props = { callback: () => void };
 
 const DeleteButton = ({ callback }: Props) => {
   return (
-    <>
-      <button
-        className="border-2 rounded-md border-black bg-red-400"
-        onClick={() => callback()}
-      >
-        delete
-      </button>
-    </>
+    <button
+      className="border-2 rounded-md border-black bg-red-400"
+      onClick={() => callback()}
+    >
+      delete
+    </button>
   );
 };
 
