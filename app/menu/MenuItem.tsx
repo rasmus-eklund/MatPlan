f3aa0fbc13a5008b8347e733f56b9d1ae4d3d515
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-23 08:44:36 +0200

    refactor: menu items and style

--- app/menu/MenuItem.tsx
+++ app/menu/MenuItem.tsx
@@ -1,31 +1,54 @@
 'use client';
-import { Recipe } from '@/types';
-import React from 'react';
-import { removeRecipeFromMenu } from '../db/prisma';
+import { MenuItem } from '@/types';
+import React, { useEffect, useState } from 'react';
+import { removeRecipeFromMenu, updateMenuPortions } from '../db/prisma';
 import DeleteButton from '../components/DeleteButton';
 
 type Props = {
-  recipe: Recipe;
+  item: MenuItem;
   callback: () => Promise<void>;
 };
 
-const MenuItem = ({ recipe, callback }: Props) => {
+const MenuItem = ({ item, callback }: Props) => {
+  const [portionsState, setPortionsState] = useState(item.portions);
   const handleRemove = async (id: string) => {
     await removeRecipeFromMenu(id);
     await callback();
   };
+  const handleMinus = () => {
+    setPortionsState(portionsState === 1 ? 1 : portionsState - 1);
+  };
+  const handlePlus = () => {
+    setPortionsState(portionsState + 1);
+  };
+  useEffect(() => {
+    (async () => {
+      await updateMenuPortions(item.id, portionsState);
+    })();
+  }, [portionsState, item]);
+
   return (
-    <li className="flex align-middle border-2 border-gray-400 p-2">
-      <p>{recipe.name}</p>
-      <div className="flex content-between">
-        <button className="rounded-full bg-red-300 w-5 h-5 align-middle">
+    <li className="flex items-center border-2 border-gray-400 p-2 gap-2">
+      <p>{item.recipe.name}</p>
+      <div className="flex content-between items-center gap-2">
+        <button
+          onClick={handleMinus}
+          className="rounded-full bg-red-300 w-5 h-5 flex items-center justify-center"
+        >
           -
         </button>
-        <p>{recipe.portions}</p>
-        <button className="rounded-full bg-green-300 w-5 h-5 align-middle">
+        <p>{portionsState}</p>
+        <button
+          onClick={handlePlus}
+          className="rounded-full bg-green-300 w-5 h-5 flex items-center justify-center"
+        >
           +
         </button>
-        <DeleteButton callback={() => {handleRemove(recipe.id)}}/>
+        <DeleteButton
+          callback={() => {
+            handleRemove(item.id);
+          }}
+        />
       </div>
     </li>
   );
