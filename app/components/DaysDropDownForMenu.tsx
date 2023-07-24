f23179f4b5169bb23cbaf37d1d1e3c2bc85e63ed
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-24 19:27:40 +0200

    fix: errors

--- app/components/DaysDropDownForMenu.tsx
+++ app/components/DaysDropDownForMenu.tsx
@@ -1,40 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import days from '../db/days';
-import Link from 'next/link';
-import { addRecipeToMenu, changeRecipeDay } from '../db/prisma';
+import { changeRecipeDay } from '../db/prisma';
 
-type Props = {}
+type Props = {};
 
-const DaysDropDownForMenu = ({ id, day }: { id: string; day:DayParams}) => {
-        const [dayState, setDayState] = useState<DayParams>
-        (day);
-        const handleChange =async (recipeId:string, day:DayParams) => {
-            await changeRecipeDay(recipeId, day)
-        }
-        return (
-          <>
-            <select
-              className="border-2 p-1.5 px-4 rounded-md border-black m-4"
-              name="day"
-              id="day"
-              value={day}
-              onChange={e => {
-                setDayState(e.target.value as string)
-                handleChange(id, day)
-            }}
-            >
-              {days.map(day => (
-                <option key={day} value={day}>
-                  {day}
-                </option>
-              ))}
-            </select>
-          </>
-        );
-      
+const DaysDropDownForMenu = ({ id, day }: { id: string; day: string }) => {
+  const [dayState, setDayState] = useState(day);
+  day;
+  const handleChange = async (recipeId: string, day: string) => {
+    await changeRecipeDay(recipeId, day);
+  };
   return (
-    <div>DaysDropDownForMenu</div>
-  )
-}
+    <select
+      className="border-2 p-1.5 px-4 rounded-md border-black m-4"
+      name="day"
+      id="day"
+      value={day}
+      onChange={async e => {
+        setDayState(e.target.value);
+        await handleChange(id, day);
+      }}
+    >
+      {days.map(day => (
+        <option key={day} value={day}>
+          {day}
+        </option>
+      ))}
+    </select>
+  );
+};
 
-export default DaysDropDownForMenu
+export default DaysDropDownForMenu;
