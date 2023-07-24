12ad3447d619f099c7cd32831fe751c28fad3087
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-24 19:20:30 +0200

    refactor: array to categories

--- app/db/stores.ts
+++ app/db/stores.ts
@@ -1,31 +1,55 @@
 'use server';
-import { Store, StoreCategory, SubcategoryItem } from '@/types';
+import { CategoryItem, Store, StorePrisma, SubcategoryItem } from '@/types';
 import getUser from './getUser';
 import { prisma } from './db';
 
-export const get = async (id: string): Promise<StoreCategory> => {
+export const get = async (id: string): Promise<Store> => {
   const userId = await getUser();
   const subCats = await prisma.subcategory.findMany({
     include: { category: true },
   });
   const store = (await prisma.store.findUnique({
     where: { id, userId },
-  })) as Store;
-  return {
-    ...store,
-    order: store.order.map((i): SubcategoryItem => {
-      const res = subCats.find(sc => sc.id === i)!;
-      return { id: res.id, category: res.category.name, name: res.name };
-    }),
-  };
+  })) as StorePrisma;
+
+  const theMap = store.order.map(i => {
+    const match = subCats.find(sc => sc.id === i)!;
+    return {
+      subCatId: match.id,
+      subCatName: match.name,
+      catName: match.category.name,
+    };
+  });
+  const initial: CategoryItem[] = [];
+  theMap.forEach(i => {
+    const subcat: SubcategoryItem = {
+      category: i.catName,
+      subcategory: i.subCatName,
+      id: i.subCatId,
+    };
+    const indexOfCat = initial.findIndex(
+      (item: CategoryItem) => i.catName === item.category
+    );
+    if (indexOfCat !== -1) {
+      initial[indexOfCat].order.push(subcat);
+    } else {
+      initial.push({ category: i.catName, order: [subcat] });
+    }
+  });
+  const out: Store = { name: store.name, id: store.id, categories: initial };
+  return out;
 };
 
-export const getAll = async () => {
+export const getAll = async (): Promise<StorePrisma[]> => {
   const userId = await getUser();
-  return (await prisma.store.findMany({ where: { userId } })) as Store[];
+  const stores = await prisma.store.findMany({
+    where: { userId },
+  });
+  console.log(stores);
+  return stores;
 };
 
-export const upsert = async (store: Store) => {
+export const upsert = async (store: StorePrisma) => {
   const userId = await getUser();
   await prisma.store.upsert({
     where: { id: store.id },
