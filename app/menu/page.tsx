b31aa29df5a3583fbb428ad1090c50a064caaf47
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-24 19:21:04 +0200

    fix: use days from array

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -3,6 +3,7 @@ import { getMenuItems } from '../db/prisma';
 import { MenuItem as MenuItemType } from '@/types';
 import React, { useEffect, useState } from 'react';
 import MenuItem from './MenuItem';
+import days from '../db/days';
 
 const Menu = () => {
   const [menu, setMenu] = useState<MenuItemType[]>([]);
@@ -14,34 +15,22 @@ const Menu = () => {
     const items = await getMenuItems();
     setMenu(items);
   };
-  const days = [
-    'Måndag',
-    'Tisdag',
-    'Onsdag',
-    'Torsdag',
-    'Fredag',
-    'Lördag',
-    'Söndag',
-  ];
+
   return (
-      <ul>
-        {days.map(day => (
-          <li key={day}>
-            <h2>{day}</h2>
-            <ul>
-              {menu
-                .filter(r => r.day === day)
-                .map(r => (
-                  <MenuItem
-                    key={r.id}
-                    item={r}
-                    callback={() => update()}
-                  />
-                ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+    <ul>
+      {days.map(day => (
+        <li key={day}>
+          <h2>{day}</h2>
+          <ul>
+            {menu
+              .filter(r => r.day === day)
+              .map(r => (
+                <MenuItem key={r.id} item={r} callback={() => update()} />
+              ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
   );
 };
 
