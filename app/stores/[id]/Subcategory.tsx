69ae105097726b6a086e16f22ff867de672e6fd7
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-24 19:24:29 +0200

    refactor: complete change of displaying store

--- app/stores/[id]/Subcategory.tsx
+++ app/stores/[id]/Subcategory.tsx
@@ -1,14 +1,23 @@
 import { SubcategoryItem } from '@/types';
+import { useState } from 'react';
 
-type Props = { items: SubcategoryItem[] };
+type Props = { items: SubcategoryItem };
 
-const Subcategory = ({ items }: Props) => {
+const Subcategory = ({ items: { category, id, subcategory } }: Props) => {
+  const [move, setMove] = useState(false);
+  const toggle = () => {
+    setMove(!move);
+  };
   return (
-    <ul>
-      {items.map(i => (
-        <li key={i.name}>{i.name}</li>
-      ))}
-    </ul>
+    <li className="flex gap-5">
+      <p onClick={toggle}>{subcategory}</p>
+      {move && (
+        <div className="flex gap-1">
+          <button onClick={() => console.log('upp')}>upp</button>
+          <button onClick={() => console.log('down')}>down</button>
+        </div>
+      )}
+    </li>
   );
 };
 
--- app/stores/[id]/page.tsx
+++ app/stores/[id]/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import * as stores from '@/app/db/stores';
-import { StoreCategory } from '@/types';
-import Categories from './Categories';
+import Category from './Category';
+import { Store } from '@/types';
 
 type Props = { params: { id: string } };
-const Store = ({ params: { id } }: Props) => {
-  const [store, setStore] = useState<StoreCategory>();
-  const [category, setCategory] = useState<String[]>([]);
+const StoreComponent = ({ params: { id } }: Props) => {
+  const [store, setStore] = useState<Store>();
 
   useEffect(() => {
     stores.get(id).then(s => {
@@ -15,32 +14,13 @@ const Store = ({ params: { id } }: Props) => {
     });
   }, [id]);
 
-  const toCategories = (store: StoreCategory) =>
-    store.order
-      .map(i => i.category)
-      .filter((item, i, ar) => ar.indexOf(item) === i);
-
   return (
     <main>
-      <h1 className="text-xl">{store?.name}</h1>
-      {store && (
-        <ul className="border-2 border-red-500">
-          {toCategories(store).map(cat => (
-            <li className="border-2 border-blue-500 ps-3" key={cat}>
-              <h2 className="text-2xl">{cat}</h2>
-              <ul>
-                {store.order
-                  .filter(i => i.category === cat)
-                  .map(({ name, category, id }) => (
-                    <li key={`${id}_${category}`}>{name}</li>
-                  ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
-      )}
+      <h1 className="text-xl">{store?.name || 'loading'}</h1>
+      {store &&
+        store.categories.map(s => <Category key={s.category} category={s} />)}
     </main>
   );
 };
 
-export default Store;
+export default StoreComponent;
