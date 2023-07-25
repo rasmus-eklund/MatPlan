29c494d498f585e5c2b5e72b25d6f2a5eb191f47
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-25 17:23:13 +0200

    feat: icons

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,32 +2,52 @@
 import React from 'react';
 import Link from 'next/link';
 import 'tailwindcss/tailwind.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboardList, faHouseChimney, faPizzaSlice, faStore, faTrashCan, faUtensils } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className='bg-1'>
       <ul className="flex flex-row m-0 max-w-full justify-center sm:justify-start">
-        <li className="font-bold py-2 underline m-0.5">
-          <Link href={'/menu'}>Meny</Link>
+        <li >
+       
+          <Link href={'/menu'}>
+          <FontAwesomeIcon className='mx-8 mt-4 px-8' icon={faHouseChimney} size='2xl' style={{color: '#DDE6ED'}}/>
+          <h3 className='mx-8 text-4 px-8 font-bold'>Hem</h3>
+          </Link>
         </li>
 
-        <li className="font-bold py-2 underline m-0.5">
-          <Link href={'/recipes'}>Maträtter</Link>
+        <li >
+          <Link href={'/recipes'}>
+          <FontAwesomeIcon className='mx-8 mt-4 px-8' icon={faUtensils} size='2xl' style={{color: '#DDE6ED'}}/>
+          <h3 className='mx-8 text-4 font-bold px-8'>Meny</h3>
+          </Link>
         </li>
 
-        <li className="font-bold py-2 underline m-0.5">
-          <Link href={'/ingredients'}>Varor</Link>
+        <li >
+          <Link href={'/ingredients'}>   <FontAwesomeIcon className='mx-8 mt-4 px-8' icon={faPizzaSlice} size='2xl' style={{color: '#DDE6ED'}}/>
+          <h3 className='mx-8 text-4 font-bold px-8'>Varor</h3>
+          </Link>
         </li>
 
-        <li className="font-bold py-2 underline m-0.5">
-          <Link href={'/shoppingList'}>Inköpslista</Link>
+        <li >
+          <Link href={'/shoppingList'}>
+          <FontAwesomeIcon className='mx-8 mt-4 px-8' icon={faClipboardList} size='2xl' style={{color: '#DDE6ED'}}/>
+          <h3 className='mx-8 text-4 font-bold px-2'>Inköpslista</h3>
+          </Link>
         </li>
 
-        <li className="font-bold py-2 underline m-0.5">
-          <Link href={'/stores'}>Butiker</Link>
+        <li >
+          <Link href={'/stores'}>
+          <FontAwesomeIcon className='mx-8 mt-4 px-8' icon={faStore} size='2xl' style={{color: '#DDE6ED'}}/>
+          <h3 className='mx-8 text-4 font-bold px-8'>Butik</h3>
+          </Link>
+
+
         </li>
       </ul>
     </nav>
+ 
   );
 };
 
