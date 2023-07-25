8f0f505334663d70db50b36cc476a7bce314eb1d
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-25 11:03:29 +0200

    feat: add new user to database

--- app/db/user.ts
+++ app/db/user.ts
@@ -1,5 +1,9 @@
+'use server';
 import { getServerSession } from 'next-auth';
 import options from '../api/auth/[...nextauth]/options';
+import { prisma } from './db';
+import { Ingredient, RecipeNoId } from '@/types';
+import defaultRecipes from './recipes/recieps';
 
 const getUser = async () => {
   const session = await getServerSession(options);
@@ -7,6 +11,56 @@ const getUser = async () => {
     throw new Error('No user');
   }
   return session.user.email;
+};
+
+export const checkNewUser = async () => {
+  const userId = await getUser();
+  const users = await prisma.user.findUnique({ where: { id: userId } });
+  console.log(users);
+  if (users === null) {
+    console.log('add new user');
+    await prisma.user.create({ data: { id: userId } });
+    await createDefaultStore(userId);
+    await createDefaultRecipes(userId);
+  }
+  console.log('User in db');
+};
+
+const createDefaultStore = async (userId: string) => {
+  const cats = await prisma.subcategory.findMany();
+  const store = cats.map(i => i.id);
+  await prisma.store.create({
+    data: { name: 'Ny affär', userId, order: store },
+  });
+};
+
+const createDefaultRecipes = async (userId: string) => {
+  for (const recipe of defaultRecipes) {
+    await addRecipe(recipe, userId);
+  }
+};
+
+const addRecipe = async (
+  { name, portions, instruction, ingredients }: RecipeNoId,
+  userId: string
+) => {
+  const newRecipe = await prisma.recipe.create({
+    data: { name, portions, instruction, userId },
+  });
+  await addRecipeIngredient(ingredients, newRecipe.id);
+};
+
+const addRecipeIngredient = async (
+  ingredients: Ingredient[],
+  recipeId: string
+) => {
+  const newIngs = ingredients.map(({ quantity, unit, name }) => ({
+    ingredientName: name,
+    recipeId,
+    quantity,
+    unit,
+  }));
+  await prisma.recipe_ingredient.createMany({ data: newIngs });
 };
 
 export default getUser;
