7ff48ca51b3cf42d29f08ff15e957b72942ca5a2
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-25 11:05:54 +0200

    feat: expandable category

--- app/stores/[id]/Category.tsx
+++ app/stores/[id]/Category.tsx
@@ -8,13 +8,20 @@ type Props = {
 
 const Category = ({ category: { category, order } }: Props) => {
   const [subOrder, setSubOrder] = useState(order);
+  const [open, setOpen] = useState(false);
   return (
-    <ul>
-      <h2 className='text-2xl underline'>{category}</h2>
-      {order.map(i => (
-        <Subcategory key={i.id} items={i} />
-      ))}
-    </ul>
+    <li className='border-2 border-blue-500'>
+      <div className="flex gap-5 items-center">
+        <h2 className="text-2xl underline flex-grow">{category}</h2>
+        <button
+          className="flex justify-center items-center rounded-full w-8 h-8 border-2 border-black"
+          onClick={() => setOpen(!open)}
+        >
+          +
+        </button>
+      </div>
+      <ul>{open && order.map(i => <Subcategory key={i.id} items={i} />)}</ul>
+    </li>
   );
 };
 
