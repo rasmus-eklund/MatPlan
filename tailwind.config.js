57661348541a0f94ec9cc089febb6a38aa885356
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-25 17:22:28 +0200

    feat: add bg img
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>
    Co-authored-by: rofunn <rofunn@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        // 'background_image':"url('./public/bg-img.jpg')"
       },
     },
   },
