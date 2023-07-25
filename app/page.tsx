e34ec87045571c49a3cc95bc13124097abcec2d3
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-25 11:00:20 +0200

    fix: welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
+import { checkNewUser } from './db/user';
 
-export default function page() {
+export default function Landing() {
+  useEffect(() => {
+    (async () => await checkNewUser())();
+  }, []);
   return (
-    <>
+    <main>
       <p>Welcome to Recipe JAR</p>
-    </>
+    </main>
   );
 }
