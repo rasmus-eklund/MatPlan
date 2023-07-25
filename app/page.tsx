a68396e87fb4c1ea48d7339139ad25af4da50391
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-25 15:19:08 +0200

    fix: remove add user on login here

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
-'use client';
-import React, { useEffect } from 'react';
-import { checkNewUser } from './db/user';
-
 export default function Landing() {
-  useEffect(() => {
-    (async () => await checkNewUser())();
-  }, []);
   return (
     <main>
       <p>Welcome to Recipe JAR</p>
