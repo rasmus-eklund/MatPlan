67d91bfb69cc77bb4a1f37a18a17a6aa2159871b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-25 15:19:31 +0200

    feat: cancel component

---
+++ app/components/Cancel.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faPencil, faX } from '@fortawesome/free-solid-svg-icons';
+
+type Props = {
+  callback: () => void;
+};
+
+const Cancel = ({ callback }: Props) => {
+  return (
+    <FontAwesomeIcon
+      type="button"
+      className={'hover:scale-110'}
+      onClick={callback}
+      icon={faX}
+      size="lg"
+      style={{ color: '#e24646' }}
+    />
+  );
+};
+
+export default Cancel;
