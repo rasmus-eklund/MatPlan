0696155b968ffa9e5854daffb5039e8f63e1af4d
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-25 20:50:38 +0200

    fix: navbar link component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,8 +3,6 @@ import React, { useEffect } from "react";
 import { randomizeLetters } from "./UIhelperfunctions";
 import Login from "./LoginButton";
 
-type Props = {};
-
 const Header = () => {
   useEffect(() => {
     const h1Element = document.querySelector("h1");
@@ -13,10 +11,10 @@ const Header = () => {
     }
   }, []);
   return (
-    <header className="bg-1 flex justify-between flex-row text-white items-center">
+    <header className="bg-1 flex justify-between text-white items-center">
       <h1
         data-value="RECIPE JAR"
-        className="bg-1  text-4 font-mono text-left p-2 py-4 text-4xl"
+        className="bg-1 text-4 font-mono text-left p-2 py-4 text-4xl"
       >
         RECIPE JAR
       </h1>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,53 +1,32 @@
 'use client';
 import React from 'react';
-import Link from 'next/link';
 import 'tailwindcss/tailwind.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClipboardList, faHouseChimney, faPizzaSlice, faStore, faTrashCan, faUtensils } from '@fortawesome/free-solid-svg-icons';
+import {
+  IconDefinition,
+  faClipboardList,
+  faHouseChimney,
+  faPizzaSlice,
+  faStore,
+  faUtensils,
+} from '@fortawesome/free-solid-svg-icons';
+import NavbarLink from './NavbarLink';
 
 const Navbar = () => {
+  const items: { name: string; href: string; icon: IconDefinition }[] = [
+    { name: 'Meny', href: '/menu', icon: faHouseChimney },
+    { name: 'Maträtter', href: '/recipes', icon: faUtensils },
+    { name: 'Varor', href: '/ingredients', icon: faPizzaSlice },
+    { name: 'Inköpslista', href: '/shoppingList', icon: faClipboardList },
+    { name: 'Butik', href: '/stores', icon: faStore },
+  ];
   return (
-    <nav className='bg-1'>
-      <ul className="flex flex-row m-0 max-w-full justify-center sm:justify-start">
-        <li >
-       
-          <Link href={'/menu'}>
-          <FontAwesomeIcon className='mx-8 mt-4 px-8' icon={faHouseChimney} size='2xl' style={{color: '#DDE6ED'}}/>
-          <h3 className='mx-8 text-4 px-8 font-bold'>Hem</h3>
-          </Link>
-        </li>
-
-        <li >
-          <Link href={'/recipes'}>
-          <FontAwesomeIcon className='mx-8 mt-4 px-8' icon={faUtensils} size='2xl' style={{color: '#DDE6ED'}}/>
-          <h3 className='mx-8 text-4 font-bold px-8'>Meny</h3>
-          </Link>
-        </li>
-
-        <li >
-          <Link href={'/ingredients'}>   <FontAwesomeIcon className='mx-8 mt-4 px-8' icon={faPizzaSlice} size='2xl' style={{color: '#DDE6ED'}}/>
-          <h3 className='mx-8 text-4 font-bold px-8'>Varor</h3>
-          </Link>
-        </li>
-
-        <li >
-          <Link href={'/shoppingList'}>
-          <FontAwesomeIcon className='mx-8 mt-4 px-8' icon={faClipboardList} size='2xl' style={{color: '#DDE6ED'}}/>
-          <h3 className='mx-8 text-4 font-bold px-2'>Inköpslista</h3>
-          </Link>
-        </li>
-
-        <li >
-          <Link href={'/stores'}>
-          <FontAwesomeIcon className='mx-8 mt-4 px-8' icon={faStore} size='2xl' style={{color: '#DDE6ED'}}/>
-          <h3 className='mx-8 text-4 font-bold px-8'>Butik</h3>
-          </Link>
-
-
-        </li>
+    <nav className="bg-1">
+      <ul className="flex justify-between px-10">
+        {items.map(({ name, href, icon }) => (
+          <NavbarLink name={name} href={href} icon={icon} key={href} />
+        ))}
       </ul>
     </nav>
- 
   );
 };
 
