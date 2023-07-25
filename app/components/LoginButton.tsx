8842839a53c61f0e691a98129333b0c255f213fd
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-25 11:02:38 +0200

    fix: login

--- app/components/LoginButton.tsx
+++ app/components/LoginButton.tsx
@@ -1,26 +1,37 @@
 'use client';
 import { useSession, signIn, signOut } from 'next-auth/react';
 import UserCard from './UserCard';
+import { checkNewUser } from '../db/user';
 
 export default function Login() {
   const { data: session } = useSession();
 
   if (session) {
     return (
-    <>
-      <div className='flex flex-row font-semibold'>
-        <button className='text-end' onClick={() => signOut()} type="button">
-          Sign out
-        </button>
-        <UserCard user={session.user} />
+      <>
+        <div className="flex flex-row font-semibold">
+          <button
+            className="text-end"
+            onClick={async () => await signOut()}
+            type="button"
+          >
+            Sign out
+          </button>
+          <UserCard user={session.user} />
         </div>
-        </>
+      </>
     );
   } else {
     return (
       <>
-        <button className='text-center' onClick={() => signIn()} type="button">
-          Sign in
+        <button
+          className="text-center"
+          onClick={ () => {
+            signIn();
+          }}
+          type="button"
+        >
+          Login
         </button>
       </>
     );
