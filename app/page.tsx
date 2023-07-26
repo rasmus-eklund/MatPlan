12ae55649679c5568965c11a1216d5e81e51f309
rofun <joufang.w@gmail.com>
2023-07-26 17:42:22 +0800

    chore: try to add bg image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 export default function Landing() {
   return (
-    <main>
+    <main className="bg-my-background-image">
       <h1>Welcome to Recipe JAR</h1>
       <h1>Log In PAGE</h1>
+      {/* <Image src="/bg_image.png" alt="background image" fill /> */}
     </main>
   );
 }
