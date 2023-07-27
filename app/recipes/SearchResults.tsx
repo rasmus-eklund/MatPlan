3871fa9098c2723e25dea3567c8b3a92491f0142
Allan Heremi <allanheremidev@gmail.com>
2023-07-27 09:03:59 +0200

    Style: searchResult ListItems

--- app/recipes/SearchResults.tsx
+++ app/recipes/SearchResults.tsx
@@ -13,10 +13,12 @@ const SearchResults = ({ recipeResult, deleteRecipe }: Props) => {
     <section>
       <ul>
         {recipeResult.map((r) => (
-          <li className="flex border-2 rounded-md border-black m-4" key={r.id}>
+          <li className="flex justify-between px-4 border-4 border-3 text-2 font-bold bg-3 rounded-md border-black m-2 items-center" key={r.id}>
             <Link href={`/recipes/${r.id}`}>{r.name}</Link>
+            <div>
             <DaysDropDown id={r.id} portions={r.portions} />
             <DeleteButton callback={() => deleteRecipe(r.id)} />
+            </div>
           </li>
         ))}
       </ul>
