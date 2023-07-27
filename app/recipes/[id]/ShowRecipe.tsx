c1f2d12ab6877a8e10017e34eb0859e1a9ffea3c
Allan Heremi <allanheremidev@gmail.com>
2023-07-27 11:05:21 +0200

    Style: WIP showRecipe

--- app/recipes/[id]/ShowRecipe.tsx
+++ app/recipes/[id]/ShowRecipe.tsx
@@ -10,23 +10,25 @@ const ShowRecipe = ({ recipe }: Props) => {
   return (
     <section className="w-full flex justify-center bg-2 p-4">
       <div className="bg-3 w-2/3 p-4 rounded-md">
-        <h1 className="text-1 font-bold text-3xl">{recipe.name}</h1>
+      <h1 className="text-1 font-bold text-3xl py-4">{recipe.name}</h1>
+        <div className='bg-2 p-2 rounded-md'>
+    
         <div className="flex justify-between">
-          <h1 className="text-2xl w-1/4 my-4">Portioner:</h1>
+          <h1 className="text-2xl w-1/4 my-4 text-4">Portioner:</h1>
           <div className="w-3/4">
-            <p className="bg-4 text-center text-2 font-bold rounded-lg w-10 my-4">
+            <p className="bg-3 text-center text-2 font-bold rounded-lg w-10 my-4">
               {recipe.portions}
             </p>
           </div>
         </div>
 
         <div className="flex  justify-between">
-          <h1 className="text-2xl">Ingredienser</h1>
+          <h1 className="text-2xl text-4">Ingredienser</h1>
           <div className=" justify-end w-3/4">
-            <ul className=" bg-4 py-2 rounded-lg text-2 ">
+            <ul className=" bg-3 py-2 rounded-lg text-2 ">
               {recipe.recipe_ingredient.map(
                 ({ id, ingredientName: name, quantity, unit }) => (
-                  <li className="bg-3 m-1.5 px-4 rounded-md" key={id}>
+                  <li className="bg-4 m-1.5 px-4 rounded-md" key={id}>
                     <div className="justify-between flex ">
                       <p>{name}</p>
                       <p>
@@ -40,13 +42,14 @@ const ShowRecipe = ({ recipe }: Props) => {
           </div>
         </div>
         <div className="flex justify-between">
-          <div className="justify-end w-1/4">
-            <h1 className="text-2xl">Instruktion</h1>
+          <div className="justify-end w-1/4 ">
+            <h1 className="text-2xl text-4">Instruktion</h1>
          
           </div>
-          <div className="w-3/4 bg-4 rounded-md mt-2 text-2 ">
-              <p className="border-4 w-3/4 rounded-md">{recipe.instruction}</p>
+          <div className="w-3/4 rounded-md mt-2 text-2 bg-3">
+              <p className=" w-3/4 rounded-md bg-3 text-2 px-4 mx-1.5">{recipe.instruction}</p>
             </div>
+        </div>
         </div>
         <label
           htmlFor="filter"
