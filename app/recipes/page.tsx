3a00512e4bc1ffcfaa39d44582dd38f21341fb55
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-07-27 15:51:27 +0200

    fix: style

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,27 +1,11 @@
 import SearchBar from "./SearchBar";
 
 const page = () => {
-  // search bar or add recipe form
-  // list of recipes
-
-  // const handleAddRecipe = async () => {
-  //   setSearch('');
-  //   const data = await getRecipeByName(search);
-  //   setRecipeResult(data);
-  // };
-
   return (
-    <main className="p-4 bg-2 flex flex-col gap-4">
+    <main className="p-4 bg-2 flex flex-col gap-4 min-h-screen">
       <SearchBar />
     </main>
   );
 };
 
 export default page;
-
-//  {addRecipe && (
-//   <>
-//     <AddRecipeForm callback={handleAddRecipe} />
-//     <button onClick={() => setAddRecipe(false)}>Tillbaka till sök</button>
-//   </>
-// )}
