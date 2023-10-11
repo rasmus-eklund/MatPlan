de4685756fa70a72e8a167e0d512c7c63fcaa6c0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-11 14:33:56 +0200

    feat: useOptimistic updates

--- app/ingredients/page.tsx
+++ app/ingredients/page.tsx
@@ -1,60 +1,96 @@
 'use client';
-import { useEffect, useState } from 'react';
+import {
+  useEffect,
+  useState,
+  experimental_useOptimistic as useOptimistic,
+} from 'react';
 import SearchIngredients from '../components/SearchIngredient';
 import { Home, ShoppingListItem } from '@/types';
 import EditIngredient from '../components/EditIngredient';
-import { getHome, addHome, removeHome } from '../db/home';
+import { editHome, getHome } from '../db/home';
 import {
   createItem,
   deleteItem,
   getShoppingList,
   updateItem,
 } from '../db/items';
+import AddHomeButton from '../components/buttons/AddHomeButton';
+import { isHome } from '../utils/utils';
+
+type Items = { items: ShoppingListItem[]; home: Home[] };
 
 const Ingredients = () => {
-  const [items, setItems] = useState<ShoppingListItem[]>([]);
-  const [itemsHome, setItemsHome] = useState<Home[]>([]);
+  const [items, setItems] = useState<Items>({ items: [], home: [] });
+  const [optimisticItems, setOptimisticItems] = useOptimistic(items);
 
   useEffect(() => {
-    Promise.all([getShoppingList(), getHome()]).then(([items, home]) => {
-      setItems(items);
-      setItemsHome(home);
+    Promise.all([getShoppingList(), getHome()]).then(([newItems, newHome]) => {
+      setItems(({ items, home }) => ({
+        items: [...items, ...newItems],
+        home: [...home, ...newHome],
+      }));
     });
   }, []);
 
-  const isHome = (name: string) => {
-    const home = Boolean(itemsHome.some(n => n.name === name));
-    if (home) {
-    }
-    return home;
-  };
-
-  const handleChange = async (check: boolean, name: string) => {
-    check ? await addHome(name) : await removeHome(name);
+  const handleHome = async (check: boolean, name: string) => {
+    setOptimisticItems(({ items, home }) =>
+      check
+        ? { items, home: [...home, { name, quantity: null, unit: null }] }
+        : { items, home: home.filter(i => i.name !== name) }
+    );
+    await editHome({ name, quantity: null, unit: null }, check);
+    setItems(({ items, home }) =>
+      check
+        ? { items, home: [...home, { name, quantity: null, unit: null }] }
+        : { items, home: home.filter(i => i.name !== name) }
+    );
   };
 
   const addIngredient = async (name: string) => {
-    const ing = await createItem({
+    const newItem: ShoppingListItem = {
       name,
       quantity: 1,
       unit: 'st',
+      checked: false,
+      from: 'extraItem',
+      id: 'placeholder',
+    };
+    setOptimisticItems(({ items, home }) => ({
+      items: [...items, { ...newItem, checked: false, id: 'placeholder' }],
+      home,
+    }));
+    const ing = await createItem({
+      name: newItem.name,
+      quantity: newItem.quantity,
+      unit: newItem.unit,
     });
-    setItems(items => [...items, ing]);
+    setItems(({ items, home }) => ({ items: [...items, ing], home }));
   };
 
-  const handleSave = async (ing: ShoppingListItem) => {
-    await updateItem(ing);
-    setItems(items => {
-      const index = items.findIndex(i => i.id === ing.id);
-      const newItems = items;
-      newItems[index] = ing;
-      return newItems;
+  const handleUpdate = async (ing: ShoppingListItem) => {
+    setOptimisticItems(({ items, home }) => {
+      const index = items.findIndex(item => item.id === ing.id);
+      items[index] = ing;
+      return { items, home };
+    });
+    const updatedItem = await updateItem(ing);
+    setItems(({ items, home }) => {
+      const index = items.findIndex(item => item.id === ing.id);
+      items[index] = updatedItem;
+      return { items, home };
     });
   };
 
   const handleDelete = async (id: string) => {
-    await deleteItem(id);
-    setItems(items => items.filter(i => i.id !== id));
+    setOptimisticItems(({ items, home }) => ({
+      items: items.filter(i => i.id !== id),
+      home,
+    }));
+    const deletedId = await deleteItem(id);
+    setItems(({ items, home }) => ({
+      items: items.filter(i => i.id !== deletedId),
+      home,
+    }));
   };
 
   return (
@@ -66,12 +102,12 @@ const Ingredients = () => {
         <div className="bg-2 p-3 rounded-md">
           <h2 className="text-3">Extra varor:</h2>
           <ul className="flex flex-col gap-2">
-            {items
+            {optimisticItems.items
               .filter(i => i.from === 'extraItem')
               .map(i => (
                 <EditIngredient
                   remove={() => handleDelete(i.id)}
-                  save={ing => handleSave({ ...i, ...ing })}
+                  update={ing => handleUpdate({ ...i, ...ing })}
                   ingredient={i}
                   key={i.id}
                   editable={true}
@@ -82,16 +118,21 @@ const Ingredients = () => {
         <div className="bg-2 p-3 rounded-md">
           <h2 className="text-3">Recept varor:</h2>
           <ul className="flex flex-col gap-2">
-            {items
+            {optimisticItems.items
               .filter(i => i.from !== 'extraItem')
               .map(i => (
                 <EditIngredient
                   remove={() => handleDelete(i.id)}
-                  save={ing => handleSave({ ...i, ...ing })}
+                  update={ing => handleUpdate({ ...i, ...ing })}
                   ingredient={i}
                   key={i.id}
                   editable={false}
-                />
+                >
+                  <AddHomeButton
+                    home={isHome(i.name, items.home)}
+                    callback={home => handleHome(home, i.name)}
+                  />
+                </EditIngredient>
               ))}
           </ul>
         </div>
